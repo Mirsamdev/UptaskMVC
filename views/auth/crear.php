916999baf0f6992e0a8d2a253820c8d663312887
Obed Miranda <xmirandadevx@gmail.com>
2024-02-22 10:19:23 -0400

    Uptask

---
+++ views/auth/crear.php
@@ -0,0 +1,35 @@
+<div class="contenedor crear">
+  <h1 class="uptask">UpTask</h1>
+  <p class="tagline">Crea y Administra tus Proyectos</p>
+
+  <div class="contenedor-sm">
+    <p class="descripcion-pagina">Iniciar Sesion</p>
+
+    <form class="formulario" method="POST" action="/">
+      <div class="campo">
+        <label for="email">Email</label>
+        <input
+            type="email"
+            id="email"
+            placeholder="Tu email"
+            name="email">
+      </div>
+
+      <div class="campo">
+        <label for="password">Password</label>
+        <input
+            type="password"
+            id="password"
+            placeholder="Tu Password"
+            name="password">
+
+      </div>
+
+      <input type="submit" class="boton" value="Iniciar Sesion">
+    </form>
+    <div class="acciones">
+      <a href="/crear">¿Aún no tienes una cuenta crea una</a>
+      <a href="/olvide">¿Olvidaste tu Password?</a>
+    </div>
+  </div> <!-- contenedor sm -->
+</div>
