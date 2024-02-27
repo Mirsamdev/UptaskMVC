898d7f5854e2ffc18716953061eea643228dbca2
Obed Miranda <xmirandadevx@gmail.com>
2024-02-27 09:27:08 -0400

    table proyectos

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -19,7 +19,12 @@ class DashboardController {
   public static function crear_proyecto(Router $router) {
     session_start();
 
+    isAuth();
+
+    $alertas = [];
+
     $router->render('dashboard/crear-proyecto', [
+      'alertas' => $alertas,
       'titulo' => 'Crear proyecto'
     ]);
   }
--- views/dashboard/crear-proyecto.php
+++ views/dashboard/crear-proyecto.php
@@ -1,5 +1,13 @@
 <?php include_once __DIR__ . '/../dashboard/header-dashboard.php';  ?>
 
+<div class="contenedor-sm">
+<?php include_once __DIR__ . '/../templates/alertas.php';  ?>
 
+<form class="formulario">
+
+<?php include_once __DIR__ . '/formulario-proyecto.php';  ?>
+  <input type="submit" value="Crear Proyecto">
+</form>
+</div>
 
 <?php include_once __DIR__ . '/../dashboard/header-footer.php';  ?>
