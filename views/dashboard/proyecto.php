991b9da89d1f884ebdffb6d284704527b0cd3e0b
Obed Miranda <xmirandadevx@gmail.com>
2024-02-29 11:47:44 -0400

    JS innerHTML Modal

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -1,5 +1,20 @@
 <?php include_once __DIR__ . '/../dashboard/header-dashboard.php';  ?>
 
-
+<div class="contenedor-sm">
+  <div class="contenedor-nueva-tarea">
+    <button
+      type="button"
+      class="agregar-tarea"
+      id="agregar-tarea"
+      >&#43;Nueva Tarea</button>
+  </div>
+</div>
 
 <?php include_once __DIR__ . '/../dashboard/header-footer.php';  ?>
+
+<?php
+ $script = '
+ <script src="build/js/tareas.js"></script>;
+ <script src="build/js/app.js"></script>
+ ';
+ ?>
